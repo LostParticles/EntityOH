af9cd3acc54b0c57580000742ea03f00d22e6a8d
IBlueSun_cp <SND\IBlueSun_cp>
2011-01-18 16:28:35 +0000

    adding support for OLE databases

---
+++ EntityOH/Controllers/Connections/SmartConnectionType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EntityOH.Controllers.Connections
+{
+    enum SmartConnectionType
+    {
+        OleConnection,
+        SqlServerConnection
+    }
+}
